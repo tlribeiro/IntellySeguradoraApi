1366c01b889ea312e4644744f0e8a3587e731bf8
tlribeiro <thiago.kcwb@gmail.com>
2019-08-11 14:19:14 -0300

    Teste deploy com AWS CLI ECS.

--- IntellySeguradoraApi/Startup.cs
+++ IntellySeguradoraApi/Startup.cs
@@ -151,7 +151,7 @@ namespace IntellySeguradoraApi
             //Define página inicial.
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("TLR - Intelly | JUNTO");
+                await context.Response.WriteAsync("TLR - Intelly | JUNTO - ECS");
             });
         }
     }
